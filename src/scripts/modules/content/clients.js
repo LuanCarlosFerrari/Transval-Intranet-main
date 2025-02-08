2e32763b539500111134c21e30353e46994eb3be
Luan Carlos Ferrari <luan.carlos.ferrari@gmail.com>
2025-02-08 20:23:56 -0300

    Refactor clients section to use a configuration object for improved maintainability and readability

--- src/scripts/modules/content/clients.js
+++ src/scripts/modules/content/clients.js
@@ -1,62 +1,69 @@
+const clientsConfig = {
+    rumo: {
+        name: 'Rumo Logística',
+        url: 'https://rumolog.com/',
+        image: 'src/Assets/clients/Rumo-logistica-logo-clipart.png'
+    },
+    ldc: {
+        name: 'LDC',
+        url: 'https://www.ldc.com/br/pt/',
+        image: 'src/Assets/clients/LDC.png'
+    },
+    viterra: {
+        name: 'Viterra',
+        url: 'https://www.viterra.us/',
+        image: 'src/Assets/clients/viterra.png'
+    },
+    caramuru: {
+        name: 'Caramuru',
+        url: 'https://caramuru.com/',
+        image: 'src/Assets/clients/caramuru.png'
+    },
+    cofco: {
+        name: 'Cofco Agri',
+        url: 'https://br.cofcointernational.com/',
+        image: 'src/Assets/clients/cofco.png'
+    },
+    raizen: {
+        name: 'Raizen',
+        url: 'https://www.raizen.com.br/',
+        image: 'src/Assets/clients/raizen.png'
+    },
+    fertipar: {
+        name: 'Fertipar',
+        url: 'https://fertipar.com.br/',
+        image: 'src/Assets/clients/fertipar.png'
+    },
+    yara: {
+        name: 'Yara',
+        url: 'https://www.yarabrasil.com.br/',
+        image: 'src/Assets/clients/yara.png'
+    },
+    copersucar: {
+        name: 'Copersucar',
+        url: 'https://www.copersucar.com.br/',
+        image: 'src/Assets/clients/copersucar.png'
+    },
+    heringer: {
+        name: 'Heringer',
+        url: 'https://www.heringer.com.br/',
+        image: 'src/Assets/clients/heringer.png'
+    }
+};
+
 export function initClientsSection() {
-    return `
+    const template = `
         <div class="titulo-container">
             <h2>Nossos Parceiros</h2>
             <div class="client-grid">
-                <div class="client-box" onclick="window.open('https://rumolog.com/', '_blank')">
-                    <img src="src/Assets/clients/Rumo-logistica-logo-clipart.png" alt="Rumo Logistica" loading="lazy">
-                    <h3>Rumo Logística</h3>
-                </div>
-                <div class="client-box" onclick="window.open('https://www.ldc.com/br/pt/', '_blank')">
-                    <img src="src/Assets/clients/LDC.png" alt="LDC">
-                    <h3>LDC</h3>
-                </div>
-                <div class="client-box" onclick="window.open('https://www.viterra.us/', '_blank')">
-                    <img src="src/Assets/clients/viterra.png" alt="Viterra">
-                    <h3>Viterra</h3>
-                </div>
-                <div class="client-box" onclick="window.open('https://caramuru.com/', '_blank')">
-                    <img src="src/Assets/clients/caramuru.png" alt="Caramuru">
-                    <h3>Caramuru</h3>
-                </div>
-                <div class="client-box" onclick="window.open('https://br.cofcointernational.com/', '_blank')">
-                    <img src="src/Assets/clients/cofco.png" alt="Cofco Agri">
-                    <h3>Cofco Agri</h3>
-                </div>
-                <div class="client-box" onclick="window.open('https://www.raizen.com.br/', '_blank')">
-                    <img src="src/Assets/clients/raizen.png" alt="Biosev">
-                    <h3>Biosev</h3>
-                </div>
-                <div class="client-box" onclick="window.open('https://fertipar.com.br/', '_blank')">
-                    <img src="src/Assets/clients/fertipar.png" alt="Fertipar">
-                    <h3>Fertipar</h3>
-                </div>
-                <div class="client-box" onclick="window.open('https://www.yarabrasil.com.br/', '_blank')">
-                    <img src="src/Assets/clients/yara.png" alt="Yara">
-                    <h3>Yara</h3>
-                </div>
-                <div class="client-box" onclick="window.open('https://www.copersucar.com.br/', '_blank')">
-                    <img src="src/Assets/clients/copersucar.png" alt="Copersucar">
-                    <h3>Copersucar</h3>
-                </div>
-                <div class="client-box" onclick="window.open('https://www.heringer.com.br/', '_blank')">
-                    <img src="src/Assets/clients/heringer.png" alt="Heringer">
-                    <h3>Heringer</h3>
-                </div>
+                ${Object.values(clientsConfig).map(client => `
+                    <a href="${client.url}" class="client-box" target="_blank" rel="noopener noreferrer">
+                        <img src="${client.image}" alt="${client.name}" loading="lazy">
+                        <h3>${client.name}</h3>
+                    </a>
+                `).join('')}
             </div>
         </div>
     `;
-}
-
-// Add event listeners if needed
-export function initClientsEvents() {
-    const clientBoxes = document.querySelectorAll('.client-box');
-    clientBoxes.forEach(box => {
-        box.addEventListener('click', handleClientBoxClick);
-    });
-}
-
-function handleClientBoxClick() {
-    const url = this.getAttribute('onclick').match(/window\.open\('([^']+)'/)[1];
-    window.open(url, '_blank');
+    return template;
 }
